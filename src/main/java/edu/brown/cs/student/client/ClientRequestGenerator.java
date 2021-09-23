ec608875f106534a10a52103bfbffcbfe68ec615
movo5hmkwxub4roi <movo5hmkwxub4roi@gmail.com>
2021-09-22 20:08:56 -0400

    feat: basic get

--- src/main/java/edu/brown/cs/student/client/ClientRequestGenerator.java
+++ src/main/java/edu/brown/cs/student/client/ClientRequestGenerator.java
@@ -20,8 +20,12 @@ public class ClientRequestGenerator {
     // The resource we want is hosted at https://cq2gahtw4j.execute-api.us-east-1.amazonaws.com/.
     String reqUri = "https://epb3u4xo11.execute-api.us-east-1.amazonaws.com/Prod/introResource";
     // TODO build and return a new GET HttpRequest.
+    HttpRequest request = HttpRequest.newBuilder()
+        .uri(URI.create("https://cq2gahtw4j.execute-api.us-east-1.amazonaws.com/"))
+        .build();
     // See https://docs.oracle.com/en/java/javase/11/docs/api/java.net.http/java/net/http/HttpRequest.html and
     // https://docs.oracle.com/en/java/javase/11/docs/api/java.net.http/java/net/http/HttpRequest.Builder.html
+    System.out.println(request);
     return null;
   }
 
